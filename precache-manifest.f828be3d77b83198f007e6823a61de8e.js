c5377d46fb12eaa0930aee96fa39a82b01fa0f71
Rémi Testa <remi.testa@gmail.com>
2019-01-26 11:18:46 +0100

    Updates

---
+++ precache-manifest.f828be3d77b83198f007e6823a61de8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23af0f27b424b25700d5",
+    "url": "/reader/static/js/runtime~main.23af0f27.js"
+  },
+  {
+    "revision": "07866ea5d1b8a0cbee29",
+    "url": "/reader/static/js/main.07866ea5.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/reader/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "07866ea5d1b8a0cbee29",
+    "url": "/reader/static/css/main.6b328217.chunk.css"
+  },
+  {
+    "revision": "7e8a195cac2fec8a1829759c979a4012",
+    "url": "/reader/index.html"
+  }
+];
